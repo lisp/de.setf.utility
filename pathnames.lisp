a61b1d24818849d4fb6c55430772f13412478c55
james anderson <james.anderson@setf.de>
2010-08-03 19:15:38 +0200

    specified translations take precedence; remove duplicates

--- pathnames.lisp
+++ pathnames.lisp
@@ -110,47 +110,82 @@
   (format nil "~a:~a" host namestring))
 
 (defun make-binary-translation-target (host)
-  (make-pathname :name :wild
-                 :type (pathname-type (compile-file-pathname ";NAME.LISP"))
-                 :version :newest
-                 :defaults
-                 (make-hosted-pathname host (format nil "root;bin;~a;**;*.*.*" (runtime-directory-name)))))
+  (make-pathname :host host :directory `(:absolute "bin" ,(runtime-directory-name) :wild-inferiors)
+                 ;; :version :newest
+                 :name :wild :type (pathname-type (compile-file-pathname "NAME.LISP"))))
+
 
 (defun set-relative-logical-pathname-translations
        (host &key (base  (or *compile-file-truename* *load-truename*))
              ((:relative-pathname location) nil)
              ((:absolute-pathname root-directory)
               (if location (merge-pathnames location base) base))
-             (root-target (make-pathname :name :wild :type :wild :version :newest
+             (root-target (make-pathname :name :wild :type :wild
                                          :directory (append (pathname-directory root-directory)
                                                             '(:wild-inferiors))
                                          :defaults root-directory))
              (translations nil))
-  (let ((bin nil))
+  (let ((bin nil)
+        #+clisp (CUSTOM:*PARSE-NAMESTRING-ANSI* t))
     (setf host (string host))
     ;; first bootstrap bin, use it to get the actual location
     ;; then install the extended translation - w/ bin mapped to a physical location
+    ;; nb. clisp fails with the error "*** - MAKE-PATHNAME: Illegal :DIRECTORY argument (:DIRECTORY)"
+    ;; if the mapping include intermediate targets, eg. ("ROOT;**;*.*.*" ,(make-pathname :version :wild :defaults root-target))
+    ;; thus the enumerated targets
     (setf (logical-pathname-translations host)
-          `(("root;**;*.*.*" ,root-target)))
+          `(("**;*.*.*" ,(make-pathname :version :wild :defaults root-target))
+            ("**;*.*" ,(make-pathname :version nil :defaults root-target))))
     (setf bin (translate-logical-pathname (make-binary-translation-target host)))
     (when *load-verbose*
       (format *trace-output* "~&Host translations: ~a~%        base: ~s.~%    location: ~s.~% root-target: ~s.~%      binary: ~s."
               host base location root-target bin))
     (setf (logical-pathname-translations host)
           ;; some implementation require the distinction in version designator
-          `(("**;*.bin" ,(make-pathname :version nil :defaults bin))
-            ("**;*.BIN" ,(make-pathname :version nil :defaults bin))
-            ("**;*.bin.*" ,bin)
-            ("**;*.BIN.*" ,bin)
-            (,(format nil "**;*.~a" *physical-binary-type*) ,(make-pathname :version nil :defaults bin))
-            (,(format nil "**;*.~a.*" *physical-binary-type*) ,(make-pathname :version :wild :defaults bin))
-            ("code;**;*.*.*" ,(make-hosted-pathname host "root;code;**;*.*.*"))
-            ("code;**;*.*" ,(make-hosted-pathname host  "root;code;**;*.*"))
-            ("root;**;*.*" ,(make-pathname :version nil :defaults root-target))
-            ("root;**;*.*.*" ,root-target)
-            ,@translations
-            ("**;*.*" ,(make-hosted-pathname host  "root;**;*.*"))
-            ))))
+          (remove-duplicates `(,@translations
+                               ("**;*.bin" ,(make-pathname :version nil :defaults bin))
+                               ("**;*.BIN" ,(make-pathname :version nil :defaults bin))
+                               ("**;*.bin.*" ,bin)
+                               ("**;*.BIN.*" ,bin)
+                               (,(format nil "**;*.~a" *physical-binary-type*) ,(make-pathname :version nil :defaults bin))
+                               (,(format nil "**;*.~a.*" *physical-binary-type*) ,bin)
+                               ("code;**;*.*" ,(make-pathname :directory `(,@(butlast (pathname-directory root-target))
+                                                                           "code"
+                                                                           ,(first (last (pathname-directory root-target))))
+                                                              :name :wild :type :wild :version nil
+                                                              :defaults root-target))
+                               ("code;**;*.*.*" ,(make-pathname :directory `(,@(butlast (pathname-directory root-target))
+                                                                             "code"
+                                                                             ,(first (last (pathname-directory root-target))))
+                                                                :name :wild :type :wild
+                                                                :defaults root-target))
+                               ("**;*.*" ,(make-pathname :directory (pathname-directory root-target)
+                                                         :name :wild :type :wild :version nil
+                                                         :defaults root-target))
+                               ("**;*.*.*" ,(make-pathname :directory (pathname-directory root-target)
+                                                           :name :wild :type :wild
+                                                           :defaults root-target))
+                               ;;("**;*.*.*" ,(make-hosted-pathname host  "ROOT;**;*.*.*"))
+                               ;;("**;*.*" ,(make-hosted-pathname host  "ROOT;**;*.*"))
+                               )
+                             :from-end t :key #'first :test #'equalp))))
+
+#+(or )
+(let ((bin-type "fas"))
+  (setf (logical-pathname-translations "ASDFTEST")
+        `(;(,(format nil "**;*.~a" bin-type)
+          ; ,(make-pathname :directory '(:absolute "development" "bin" :wild-inferiors) :name :wild :type bin-type :version nil))
+          ;(,(format nil "**;*.~a.*" bin-type)
+          ; ,(make-pathname :directory '(:absolute "development" "bin" :wild-inferiors) :name :wild :type bin-type))
+          ("**;*.*"
+           ,(make-pathname :directory '(:absolute "development" "source" :wild-inferiors) :name :wild :type :wild :version nil))
+          ("**;*.*.*"
+           ,(make-pathname :directory '(:absolute "development" "source" :wild-inferiors) :name :wild :type :wild)))))
+
+#+allegro
+(defun logical-hosts ()
+  (loop for host being each hash-key of excl::*logical-pathname-translations*
+        collect host))
 
 ;;; both clozure and digitool bind host definitions as an alist
 #+ccl
@@ -159,20 +194,35 @@
 #+ccl
 (defun logical-hosts ()
   (mapcar #'first (logical-hosts-translations)))
-#+lispworks
+
+#+(or clisp lispworks)
 (defun logical-hosts ()
   (loop for host being each hash-key of system::*logical-pathname-translations*
         collect host))
+
+#+cmu
+(defun logical-hosts ()
+  (loop for host being each hash-key of lisp::*logical-hosts*
+        collect host))
+
+#+ecl
+;; as per jj.garcia-ripoll
+(progn
+  (eval-when (:execute)
+    (setf (symbol-function 'logical-hosts)
+          (compile nil '(lambda ()
+                          (mapcar #'first (ffi:c-inline () () :object "cl_core.pathname_translations" :one-liner t))))))
+
+  (eval-when (:compile-toplevel :load-toplevel)
+    (defun logical-hosts ()
+      (mapcar #'first (ffi:c-inline () () :object "cl_core.pathname_translations" :one-liner t)))))
+
 #+sbcl
 (defun logical-hosts ()
   (loop for host being each hash-key of SB-IMPL::*LOGICAL-HOSTS*
         collect host))
-#+allegro
-(defun logical-hosts ()
-  (loop for host being each hash-key of excl::*logical-pathname-translations*
-        collect host))
 
-#-(or allegro ccl lispworks sbcl)
+#-(or allegro ccl clisp lispworks sbcl ecl cmu)
 (defun logical-hosts ()
   (cerror "Assume no logical hosts." "This runtime has no definition for ~s." 'logical-hosts)
   nil)
@@ -202,6 +252,7 @@
       (flet ((record-candidate (host enough)
                (let* ((candidate (make-pathname :host host :directory (cons :absolute (rest (pathname-directory enough)))
                                                 :name (pathname-name pathname) :type (pathname-type pathname)
+                                                :version (pathname-version pathname)
                                                 :defaults enough))
                       (retranslated-candidate (ignore-errors (translate-logical-pathname candidate))))
                  (when (and (equalp pathname retranslated-candidate)
@@ -223,13 +274,14 @@
                           (setf specific-enough enough)
                           (setf translated candidate)))))))
         (dolist (host (logical-hosts))
-          (let ((proto-translation (ignore-errors (translate-logical-pathname (concatenate 'string host ":TEST.LISP")))))
+          (let ((proto-translation (ignore-errors (translate-logical-pathname
+                                                   (make-pathname :host host :directory '(:absolute) :name "TEST" :type "LISP")))))
             (when proto-translation
               (let* ((host-translation (make-pathname :name nil :type nil :defaults proto-translation))
                      (enough (enough-namestring pathname host-translation)))
                 ;; if the pathname is "hosted", check if it is commensurable with already found
                 ;; if yes, retain the more specific host. if no, signal an error
-                ;; if non yet, cache this one
+                ;; if non yet, cache this one 
                 (unless (equal enough namestring)
                   (record-candidate host enough)))))))
       translated)))
@@ -257,27 +309,34 @@
   
   (or (ignore-errors (logical-pathname-translations "LIBRARY"))
       (macrolet ((source-pathname () (truename (or *compile-file-pathname* *load-pathname*))))
-        (define-library-host (source-pathname)))))
+        #+cmu (handler-bind ((error (lambda (c) (warn "redefining LIBRARY: ~a." c) (continue))))
+                (define-library-host (source-pathname)))
+        #-cmu (define-library-host (source-pathname)))))
 
-
-(let ((production (merge-pathnames (make-pathname :directory '(:relative :up :up "production" "Library"))
-                                   (truename #p"LIBRARY:"))))
-  (when (probe-file production)
+;; nb. clisp neither merges :up relative pathnames, nor (by default) correctly parses logical namestrings
+(let* ((library (truename (make-pathname :host "LIBRARY" :directory '(:absolute))))
+       (production (make-pathname :directory (append (butlast (pathname-directory library) 2)
+                                                     '("production" "Library"))
+                                  :name nil :type nil :defaults library)))
+  (when (#-clisp probe-file #+clisp ext:probe-directory production)
     (set-relative-logical-pathname-translations "P-LIBRARY" :absolute-pathname production)))
            
 
-(unless (let* ((logical #P"LIBRARY:de;setf;utility;pathnames.lisp")
-               (physical (translate-logical-pathname logical))
-               (back (translate-physical-pathname physical)))
-          ;; test equality for the primary attributes
-          ;; and control for uniqueness
-          (and (equalp (pathname-host back) (pathname-host logical))
-               (equal (pathname-directory back) (pathname-directory logical))
-               (equalp (pathname-name back) (pathname-name logical))
-               (equalp (pathname-type back) (pathname-type logical))
-               (null (translate-physical-pathname (make-pathname :type "xxx" :defaults physical)
-                                                  :if-does-not-exist nil))))
-  (warn "translate-physical-pathname ?"))
+(let* ((logical (make-pathname :host "LIBRARY" :directory '(:absolute "de" "setf" "utility")
+                               :name "pathnames" :type "lisp"))
+       (physical (translate-logical-pathname logical))
+       (back (translate-physical-pathname physical)))
+  ;; test equality for the primary attributes
+  ;; and control for uniqueness
+  (let ((result (when back
+                  (list (equalp (pathname-host back) (pathname-host logical))
+                        (equal (pathname-directory back) (pathname-directory logical))
+                        (equalp (pathname-name back) (pathname-name logical))
+                        (equalp (pathname-type back) (pathname-type logical))
+                        (null (translate-physical-pathname (make-pathname :type "xxx" :defaults physical)
+                                                           :if-does-not-exist nil))))))
+    (when (or (null result) (member nil result))
+      (warn "translate-physical-pathname failed: ~s" result))))
 ;(trace translate-physical-pathname)
 ;(translate-physical-pathname (translate-logical-pathname #P"LIBRARY:de;setf;utility;pathnames.lisp"))
 :de.setf.utility
