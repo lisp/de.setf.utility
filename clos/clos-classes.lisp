5f6d0fa851d5f715f3534089ffb8a6e7609823f9
james anderson <james.anderson@setf.de>
2012-02-09 14:33:28 +0100

    clean dynamic-extent declarations

--- clos/clos-classes.lisp
+++ clos/clos-classes.lisp
@@ -101,9 +101,7 @@
          (class-variable-name (cons-symbol package :*class. class-name "*"))
          (predicate-name (or (second (assoc :predicate options))
                              (cons-symbol package class-name :-p)))
-         (rest-arg (second (member '&rest lambda-list)))
-         (declaration (when (and rest-arg (not (assoc 'declare options)))
-                        `((declare (dynamic-extent ,rest-arg))))))
+         (rest-arg (second (member '&rest lambda-list))))
     ;; iff the keyword parameters are specified, (at least for mcl) one must specify
     ;; &allow-other-keys in order to permit varied argument forms to the symbol
     ;; method
@@ -126,7 +124,6 @@
          (:method ((datum t)) nil)
          (:method ((datum ,class-name)) t))
        (defgeneric ,constructor-name ,lambda-list
-         ,@declaration
          ,(or (assoc :documentation options)
              `(:documentation
                ,(format nil "Construct a ~a given initialization arguments. The
